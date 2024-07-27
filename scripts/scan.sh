542ce49d887e4c0bf0b431ac9f709017ad88ac48
Rodrigo Scola <rodrigo.sgarabotto.scola@gmail.com>
2024-07-27 17:46:03 -0300

    P:

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
 grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
 echo "========= CREDIT CARD SCAN COMPLETE =========="
@@ -9,3 +10,6 @@ grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} 
 grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
 echo "========= PHONE NUMBER SCAN COMPLETE ========="
 
+=======
+# TODO: write the script
+>>>>>>> parent of 81cca7e (K:)
